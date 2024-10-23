f6c85e8c88e5fab18bdc51d5c79618e6ffc4a2c4
Pan Ziyue <random.rrr3@gmail.com>
2024-10-23 13:58:31 +0800

    revamped UI

--- plugins/impart_gui.py
+++ plugins/impart_gui.py
@@ -17,19 +17,12 @@ import wx.xrc
 class impartGUI ( wx.Dialog ):
 
     def __init__( self, parent ):
-        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"KiCad Importer", pos = wx.DefaultPosition, size = wx.Size( 650,650 ), style = wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.BORDER_DEFAULT )
+        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"KiCad Importer", pos = wx.DefaultPosition, size = wx.Size( 655,635 ), style = wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.BORDER_DEFAULT )
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
         self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
 
         bSizer = wx.BoxSizer( wx.VERTICAL )
-
-        self.m_button_migrate = wx.Button( self, wx.ID_ANY, u"migrate the libraries (highly recommended)", wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.m_button_migrate.SetFont( wx.Font( 15, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString ) )
-        self.m_button_migrate.Hide()
-        self.m_button_migrate.SetMaxSize( wx.Size( -1,150 ) )
-
-        bSizer.Add( self.m_button_migrate, 1, wx.ALL|wx.EXPAND, 5 )
 
         self.m_staticText_librarypath2 = wx.StaticText( self, wx.ID_ANY, u"Console", wx.DefaultPosition, wx.DefaultSize, 0 )
         self.m_staticText_librarypath2.Wrap( -1 )
@@ -61,26 +54,33 @@ class impartGUI ( wx.Dialog ):
 
         bSizer.Add( self.m_staticText7, 0, wx.ALL, 5 )
 
-        fgSizer21 = wx.FlexGridSizer( 3, 2, 0, 0 )
-        fgSizer21.AddGrowableCol( 2 )
-        fgSizer21.AddGrowableRow( 3 )
-        fgSizer21.SetFlexibleDirection( wx.HORIZONTAL )
-        fgSizer21.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_NONE )
+        m_radioBox_sourceChoices = [ u"SamacSys / Ultralibrarian / Octopart (manually download zip file)", u"LCSC (key in LCSC part no.)" ]
+        self.m_radioBox_source = wx.RadioBox( self, wx.ID_ANY, u"Source", wx.DefaultPosition, wx.DefaultSize, m_radioBox_sourceChoices, 1, wx.RA_SPECIFY_COLS )
+        self.m_radioBox_source.SetSelection( 0 )
+        bSizer.Add( self.m_radioBox_source, 0, wx.ALL|wx.EXPAND, 0 )
 
-        self.m_button_selectzip = wx.Button( self, wx.ID_ANY, u"Select Zip File from SamacSys/Ultralibrarian", wx.DefaultPosition, wx.DefaultSize, 0 )
-        fgSizer21.Add( self.m_button_selectzip, 1, wx.ALL|wx.EXPAND, 5 )
+        self.m_staticText_zipfileloc = wx.StaticText( self, wx.ID_ANY, u"Zip File Location", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText_zipfileloc.Wrap( -1 )
+
+        bSizer.Add( self.m_staticText_zipfileloc, 0, wx.ALL, 5 )
 
         self.m_dirPicker_sourcepath = wx.FilePickerCtrl( self, wx.ID_ANY, wx.EmptyString, u"Select a file", u"*.*", wx.DefaultPosition, wx.Size( -1,-1 ), wx.FLP_DEFAULT_STYLE )
-        fgSizer21.Add( self.m_dirPicker_sourcepath, 2, wx.ALL|wx.EXPAND, 5 )
+        self.m_dirPicker_sourcepath.SetMaxSize( wx.Size( -1,20 ) )
 
-        self.m_button_selectlcsc = wx.Button( self, wx.ID_ANY, u"Select LCSC #", wx.DefaultPosition, wx.DefaultSize, 0 )
-        fgSizer21.Add( self.m_button_selectlcsc, 1, wx.ALL|wx.EXPAND, 5 )
+        bSizer.Add( self.m_dirPicker_sourcepath, 2, wx.ALL|wx.EXPAND, 5 )
 
-        self.m_textCtrl_lcsc_number = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 400,-1 ), wx.TE_PROCESS_ENTER )
-        fgSizer21.Add( self.m_textCtrl_lcsc_number, 2, wx.ALL|wx.EXPAND, 5 )
+        self.m_staticText_lcscpartno = wx.StaticText( self, wx.ID_ANY, u"LCSC Part Number", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText_lcscpartno.Wrap( -1 )
 
+        self.m_staticText_lcscpartno.Hide()
 
-        bSizer.Add( fgSizer21, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.EXPAND, 5 )
+        bSizer.Add( self.m_staticText_lcscpartno, 0, wx.ALL, 5 )
+
+        self.m_textCtrl_lcsc_number = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,-1 ), wx.TE_PROCESS_ENTER )
+        self.m_textCtrl_lcsc_number.Hide()
+        self.m_textCtrl_lcsc_number.SetMaxSize( wx.Size( -1,20 ) )
+
+        bSizer.Add( self.m_textCtrl_lcsc_number, 2, wx.ALL|wx.EXPAND, 5 )
 
         self.m_staticline121 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
         self.m_staticline121.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
@@ -107,7 +107,7 @@ class impartGUI ( wx.Dialog ):
         fgSizer11.Add( self.m_staticText13, 0, wx.ALL, 5 )
 
         self.m_textCtrl_prefix = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.m_textCtrl_prefix.SetMinSize( wx.Size( 380,-1 ) )
+        self.m_textCtrl_prefix.SetMinSize( wx.Size( 390,-1 ) )
 
         fgSizer11.Add( self.m_textCtrl_prefix, 0, wx.ALL, 5 )
 
@@ -117,7 +117,7 @@ class impartGUI ( wx.Dialog ):
         fgSizer11.Add( self.m_staticText131, 0, wx.ALL, 5 )
 
         self.m_textCtrl_partno = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.m_textCtrl_partno.SetMinSize( wx.Size( 380,-1 ) )
+        self.m_textCtrl_partno.SetMinSize( wx.Size( 390,-1 ) )
 
         fgSizer11.Add( self.m_textCtrl_partno, 0, wx.ALL, 5 )
 
@@ -140,12 +140,14 @@ class impartGUI ( wx.Dialog ):
         self.m_overwrite = wx.CheckBox( self, wx.ID_ANY, u"Overwrite existing entry in library", wx.DefaultPosition, wx.DefaultSize, 0 )
         bSizer.Add( self.m_overwrite, 0, wx.ALL, 5 )
 
-        self.m_staticText_librarypath = wx.StaticText( self, wx.ID_ANY, u"Library location (where you stored your library):", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText_librarypath = wx.StaticText( self, wx.ID_ANY, u"Library location (same as ${KICAD_3RD_PARTY}):", wx.DefaultPosition, wx.DefaultSize, 0 )
         self.m_staticText_librarypath.Wrap( -1 )
+
+        self.m_staticText_librarypath.SetFont( wx.Font( wx.NORMAL_FONT.GetPointSize(), wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString ) )
 
         bSizer.Add( self.m_staticText_librarypath, 0, wx.ALL, 5 )
 
-        self.m_dirPicker_librarypath = wx.DirPickerCtrl( self, wx.ID_ANY, u".", u"Select a folder", wx.DefaultPosition, wx.DefaultSize, wx.DIRP_DEFAULT_STYLE )
+        self.m_dirPicker_librarypath = wx.DirPickerCtrl( self, wx.ID_ANY, wx.EmptyString, u"Select a folder", wx.DefaultPosition, wx.DefaultSize, wx.DIRP_DEFAULT_STYLE )
         bSizer.Add( self.m_dirPicker_librarypath, 0, wx.ALL|wx.EXPAND, 5 )
 
         self.m_staticText_librarypath1 = wx.StaticText( self, wx.ID_ANY, u"Library name", wx.DefaultPosition, wx.DefaultSize, 0 )
@@ -171,6 +173,13 @@ class impartGUI ( wx.Dialog ):
         self.m_textCtrl_libname = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
         bSizer.Add( self.m_textCtrl_libname, 0, wx.ALL|wx.EXPAND, 5 )
 
+        self.m_button_migrate = wx.Button( self, wx.ID_ANY, u"Migrate the libraries (highly recommended)", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_button_migrate.SetFont( wx.Font( 15, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString ) )
+        self.m_button_migrate.Hide()
+        self.m_button_migrate.SetMaxSize( wx.Size( -1,150 ) )
+
+        bSizer.Add( self.m_button_migrate, 0, wx.ALL|wx.EXPAND, 5 )
+
         self.m_button = wx.Button( self, wx.ID_ANY, u"Import!", wx.DefaultPosition, wx.DefaultSize, 0 )
         bSizer.Add( self.m_button, 0, wx.ALL|wx.EXPAND, 5 )
 
@@ -182,11 +191,9 @@ class impartGUI ( wx.Dialog ):
 
         # Connect Events
         self.Bind( wx.EVT_CLOSE, self.on_close )
-        self.m_button_migrate.Bind( wx.EVT_BUTTON, self.migrate_libs )
-        self.m_button_selectzip.Bind( wx.EVT_BUTTON, self.ButtomManualImport )
-        self.m_button_selectlcsc.Bind( wx.EVT_BUTTON, self.ButtomManualImport )
-        self.m_textCtrl_lcsc_number.Bind( wx.EVT_TEXT_ENTER, self.ButtomManualImport )
+        self.m_radioBox_source.Bind( wx.EVT_RADIOBOX, self.RadioBoxPressed )
         self.m_dirPicker_librarypath.Bind( wx.EVT_DIRPICKER_CHANGED, self.DirChange )
+        self.m_button_migrate.Bind( wx.EVT_BUTTON, self.migrate_libs )
         self.m_button.Bind( wx.EVT_BUTTON, self.BottonClick )
 
     def __del__( self ):
@@ -197,15 +204,13 @@ class impartGUI ( wx.Dialog ):
     def on_close( self, event ):
         event.Skip()
 
-    def migrate_libs( self, event ):
+    def RadioBoxPressed( self, event ):
         event.Skip()
-
-    def ButtomManualImport( self, event ):
-        event.Skip()
-
-
 
     def DirChange( self, event ):
+        event.Skip()
+
+    def migrate_libs( self, event ):
         event.Skip()
 
     def BottonClick( self, event ):
